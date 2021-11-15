166c696a3633099deb06fde43a34b79ff89e33ed
Rocío <rocioorubio17@gmail.com>
2021-11-15 10:55:17 +0100

    primera subida

---
+++ src/test/java/com/ntt/microservice/miprimermicroservicio/MiprimermicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.microservice.miprimermicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiprimermicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
